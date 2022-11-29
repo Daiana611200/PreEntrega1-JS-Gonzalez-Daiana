cfe8aa97226eb07a7dc34a79488de5bc91526b93
Daiana <daiana_gonzalez@hotmail.com>
2022-11-28 23:58:34 -0300

    turnos

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ saludar();
 //Datos primarios
     let nombre = prompt("Ingrese nombre del paciente");
     let apellido = prompt("Ingrese apellido del paciente");
+    let dni = prompt("Ingrese DNI del paciente")
     let mensaje = `Hola ${nombre} ${apellido}`;
     alert(mensaje);
 
@@ -19,25 +20,22 @@ if(respuesta === "si"){
       alert("Solicite turno");
     }
 
-  /*  esto esta mal
- let entrada = prompt("Ingrese DNI");
+ let entrada = prompt("Ingrese Celular");
     while(entrada != "ESC"){
         let mensaje = `El usuario ingreso ${entrada}`;
-        alert(mensaje);
-
+        entrada = prompt ("Ingrese domicilio del paciente");
+        entrada = prompt ("Ingrese Fecha de nacimiento del paciente");
+        entrada = prompt ("Ingrese localidad del paciente");
+        entrada = prompt ("Ingrese Obra Social del paciente");
        alert("Solicite turno")  
        break;
       }
-    */ 
-
-         
-
-
-    // solicitud de turnos ... falta
+  
+    // solicitud de turnos 
 
     for(let turno = 1; turno <= 5; turno++){
-        let nombre = prompt("Ingrese su nombre para asignar un turno");
-        let mensaje = `Turno #${turno} Nombre: ${nombre}`;
+        let nombre = prompt("Ingrese nombre y apellido del paciente para asignar un turno");
+        let mensaje = `Turno #${turno} Nombre: ${nombre} Apellido: ${apellido}`;
         alert(mensaje);
       }
       alert("Turnos agotados");
